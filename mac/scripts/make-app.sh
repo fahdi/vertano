14f61157bb8c4c3765b1e912f204ec0b9ad1d027
fahdi <info@fahdmurtaza.com>
2026-07-27 12:02:52 +0500

    release: v0.3.9 (live speaking-rate readout)
    Bump mac + desktop to 0.3.9, refresh download links, add the speaking-rate
    feature to the site, rebuild docs/.

--- mac/scripts/make-app.sh
+++ mac/scripts/make-app.sh
@@ -39,7 +39,7 @@ cat > "$APP/Contents/Info.plist" <<'PLIST'
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>0.3.8</string>
+    <string>0.3.9</string>
     <key>CFBundleVersion</key>
     <string>1</string>
     <key>LSMinimumSystemVersion</key>
